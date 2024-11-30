0a51319337245bcfeec24af40479c192d41e26a8
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2024-11-30 15:29:17 +0400

    Add Element Plus

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,29 @@
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint';
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/christmas-trees/',
-  plugins: [vue(), eslintPlugin(), vueDevTools()],
+  base: "/christmas-trees/",
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
