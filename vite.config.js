e894f574591aeb996b8b1e9da7b84c813f8b22c2
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2024-12-01 16:02:58 +0400

    Add https

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ import eslintPlugin from "vite-plugin-eslint";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import mkcert from "vite-plugin-mkcert";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/christmas-trees/",
+  server: { https: true },
   plugins: [
     vue(),
     eslintPlugin(),
@@ -20,6 +22,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    mkcert(),
   ],
   resolve: {
     alias: {
