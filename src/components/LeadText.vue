f9648110c31ca4f25145765f5381651bc05ea53f
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2024-12-02 11:18:41 +0400

    Add LeadText

---
+++ src/components/LeadText.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-text
+    class="lead"
+    :style="{ color: colorText == 'white' ? '#FFF' : '#000' }"
+  >
+    {{ text }}
+  </el-text>
+</template>
+
+<script>
+export default {
+  name: "LeadText",
+  props: {
+    text: String,
+    colorText: String,
+  },
+};
+</script>
