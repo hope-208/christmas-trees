f420933f8f4f040323050309ada1dc85de5c4c34
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2024-12-02 11:38:23 +0400

    Add GiftBox and other style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
-  <el-container direction="vertical">
-    <HeaderItem />
-    <el-main></el-main>
-  </el-container>
+  <el-scrollbar>
+    <el-container direction="vertical">
+      <HeaderItem />
+      <el-main class="main">
+        <!-- <SwiperProgramms /> -->
+        <CarouselItem />
+        <LeadText
+          text="Ёлки на Афише как на рождественской ярмарке — всех размеров: для самых
+          маленьких, школьников и подростков, весёлые, мультяшные и сказочные. А
+          под Ёлками ещё и подарки!"
+          color-text="white"
+          style="margin: 185px 0 160px"
+        />
+
+        <LeadText text="А какие?" color-text="black" />
+
+        <GiftBox />
+        <LeadText
+          text="Как выиграть новогодний детский бокс?"
+          color-text="black"
+        />
+        <LeadText text="И это ещё не все подарки!" color-text="black" />
+      </el-main> </el-container
+  ></el-scrollbar>
 </template>
 
 <script setup>
 import HeaderItem from "@/components/HeaderItem.vue";
+import CarouselItem from "@/components/CarouselItem.vue";
+// import SwiperProgramms from "@/components/SwiperProgramms.vue";
+import LeadText from "@/components/LeadText.vue";
+import GiftBox from "@/components/GiftBox.vue";
 </script>
---
+++ src/components/GiftBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="gift">
+    <div class="gift__card">
+      <img class="gift__image" src="../assets/img/gift.svg" />
+      <el-text class="gift__info title"
+        >Еженедельный розыгрыш
+
+        <span class="gift__info-span"> боксов с подарками </span> для детей и
+        родителей
+      </el-text>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GiftBox",
+};
+</script>
