d314f42457feda17c17277fc236e54610a0e140d
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2024-12-06 00:56:54 +0400

    Adaptive on GiftBox

--- src/components/GiftBox.vue
+++ src/components/GiftBox.vue
@@ -3,9 +3,9 @@
     <div class="gift__card">
       <img class="gift__image" src="@/assets/img/gift.svg" />
       <el-text class="gift__info title"
-        >Еженедельный розыгрыш
-        <span class="gift__info-span"> боксов с&nbsp;подарками </span> для детей
-        и родителей
+        >Еженедельный розыгрыш&nbsp;<span class="gift__info-span"
+          >боксов с&nbsp;подарками</span
+        >&nbsp;для детей&nbsp;и&nbsp;родителей
       </el-text>
     </div>
   </div>
