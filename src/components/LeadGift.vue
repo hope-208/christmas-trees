a0f9fe32d98b5b8e4f9d56e2a03ed6fa1b245a94
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2024-12-13 21:19:11 +0400

    Content edits from the customer, edit adaptive slider with content programm

--- src/components/LeadGift.vue
+++ src/components/LeadGift.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="step step-lead">
     <el-row>
-      <el-tag class="step__tag">Москино ВДНХ</el-tag>
-      <el-tag class="step__tag">6 января</el-tag></el-row
+      <el-tag class="step__tag">Иллюзион</el-tag>
+      <el-tag class="step__tag">5 января</el-tag></el-row
     >
     <div class="step__content">
       <el-text class="step__header"
@@ -11,7 +11,7 @@
           style="color: #54acff"
         >
           для детей и родителей </el-text
-        >от&nbsp;Афиши и Москино*</el-text
+        >от&nbsp;Афиши в кинотеатре «Иллюзион»*</el-text
       >
       <el-text class="step__desc step__desc_accent"
         >*только для тех, кто купил билеты на ёлки в Афише</el-text
